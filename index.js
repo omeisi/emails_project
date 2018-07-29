ea95eefeffb8ce8dea809c965aba6b76d0322483
Aviad <aviad339833@gmail.com>
2018-07-29 16:00:16 -0400

    secrete and client key

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ name: 'aviad' });
-});
+
+passport.use(new GoogleStrategy());
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
+//clientID 501401801625-m1gi04b2c0b92il9774b9appnfdg1jce.apps.googleusercontent.com
+// client secret cJEVbp9dQ58Yoc6sMiEGfzoe
